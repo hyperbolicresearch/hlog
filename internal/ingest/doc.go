441529153729d048589d825e4acdd43950392856
Nahum Maurice <nahum.maurice.nm@gmail.com>
2024-02-29 06:51:28 +0300

    Changes in the logic of the ingestion pipeline

--- internal/ingest/doc.go
+++ internal/ingest/doc.go
@@ -1,12 +1,22 @@
 package ingest
 
-// ingest implements the flow from Kafka to ClickHouse. It consists of
+// ingest implements the flow from Kafka to storages. It consists of
 // two parts, which are:
-//  1. The ingester consumes from Kafka, transforms the data to
-//     the structure that will be stored in clickHouse, extracts
-//     the schema from the batch of data it buffers and generate SQL.
-//  2. The batcher is responsible to accumulate the data processed by
-//     the ingester and to batch them to ClickHouse.
+//  1. Ingest to MongoDB.
 //
-// The flow is then:
-// kafka -> ingester -> batcher -> clickhouse.
+//     Ingest into MongDB is the default real-time ingestion pipeline.
+//     As soon as a new log gets added to Kafka, we consume it and
+//     proceed with MongoDB, which notifies us on inserting in order for
+//     us to notify the clients. Like this, we have near instant notifications
+//     and real-time log monitoring.
+//
+//  2. Ingest to ClickHouse.
+//
+//     Ingestion into ClickHouse is for analytical purposes only. It is
+//     done periodically and the period is parameterizable. Upon inserting
+//     (into MongoDB), we ingest the logs to Kafka where they get accumulated
+//     for the <period> amount of time. Ingestion into ClickHouse happens
+//     by batches which allows to update metrics and query results.
+//
+//  Flow
+//  Client -> Kafka -> MongoDB -> Client & Kafka -> ClickHouse <- Client
