a72db32f04493f1193ed0744766169b01f5058cc
Nahum Maurice <nahum.maurice.nm@gmail.com>
2024-03-31 02:09:19 +0300

    Added test for clickhouse, docker compose in actions

--- storage/clickhouse/clickhouse_test.go
+++ storage/clickhouse/clickhouse_test.go
@@ -1,1 +1,11 @@
 package clickhouseservice
+
+import "testing"
+
+func TestConn(t *testing.T) {
+	addrs := []string{"localhost:9000"}
+	_, err := Conn(addrs)
+	if err != nil {
+		t.Error(err)
+	}
+}
